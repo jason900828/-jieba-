02d26a0878f2ecb61249245c40c042356585983a
jason900828 <34247377+jason900828@users.noreply.github.com>
2018-10-19 16:13:18 +0800

    Add files via upload

--- phpadddict/add_del.php
+++ phpadddict/add_del.php
@@ -1,7 +1,6 @@
 <?php
-
 $myfile = fopen("../all_dict/del_words.txt", "w")or die("Unable to open file!");
-$txt = $_GET["del_word"];
+$txt = $_POST["del_word"];
 $txt = mb_convert_encoding($txt, 'utf-8');
 fwrite($myfile, $txt);
 fclose($myfile);
